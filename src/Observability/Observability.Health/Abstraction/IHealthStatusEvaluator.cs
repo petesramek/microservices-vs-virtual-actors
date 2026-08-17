aa8f1f389a3ebe9a56ef73698bffd9332980f3da
Pete Sramek <2333452+petesramek@users.noreply.github.com>
2026-08-17 17:12:38 +0200

    added XML docs

--- src/Observability/Observability.Health/Abstraction/IHealthStatusEvaluator.cs
+++ src/Observability/Observability.Health/Abstraction/IHealthStatusEvaluator.cs
@@ -1,5 +1,15 @@
 namespace Observability.Health.Abstraction;
 
+/// <summary>
+/// Defines an operation for aggregating health statuses into one overall
+/// health status.
+/// </summary>
 public interface IHealthStatusEvaluator {
+    /// <summary>
+    /// Evaluates a collection of health statuses and returns their aggregate
+    /// status.
+    /// </summary>
+    /// <param name="statuses">The health statuses to evaluate.</param>
+    /// <returns>The aggregate health status.</returns>
     HealthStatus Evaluate(IReadOnlyCollection<HealthStatus> statuses);
 }
